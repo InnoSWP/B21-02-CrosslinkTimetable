69790b8de49066d2e8734932cee995b39f5b61a7
Ilnar <4sitescarp@gmail.com>
2022-06-25 21:03:06 +0300

    PEP8 fix in tests

--- REST_API/CrosslinkTimetable/WideEvents/tests/test_urls.py
+++ REST_API/CrosslinkTimetable/WideEvents/tests/test_urls.py
@@ -8,11 +8,11 @@ class TestUrls(SimpleTestCase):
     def test_createevent_url_is_resolved(self):
         url = reverse('create-event')
         self.assertEqual(resolve(url).func, create_event)
-    
+
     def test_welcome_url_is_resolved(self):
-        url = reverse('welcome') 
+        url = reverse('welcome')
         self.assertEqual(resolve(url).func.view_class, EventAPIView)
-    
+
     def test_profile_url_is_resolved(self):
         url = reverse('profile')
         self.assertEqual(resolve(url).func, profile)
--- REST_API/CrosslinkTimetable/WideEvents/tests/test_views.py
+++
@@ -1,7 +0,0 @@
-from django.test import SimpleTestCase
-
-from WideEvents.models import Event
-
-# class PushWideEventTest(SimpleTestCase):
-    # def test_emptyName(self):
-        # Event.objects.create(name="") 
