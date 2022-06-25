41c73dd752699417f4f498ce1768c1b073253fcb
Ilnar <4sitescarp@gmail.com>
2022-06-25 21:10:54 +0300

    Fixed test_urls.py

--- REST_API/CrosslinkTimetable/WideEvents/tests/test_urls.py
+++ REST_API/CrosslinkTimetable/WideEvents/tests/test_urls.py
@@ -6,13 +6,13 @@ from WideEvents.views import EventAPIView, create_event, profile
 
 class TestUrls(SimpleTestCase):
     def test_createevent_url_is_resolved(self):
-        url = reverse('create-event')
+        url = reverse("create-event")
         self.assertEqual(resolve(url).func, create_event)
 
     def test_welcome_url_is_resolved(self):
-        url = reverse('welcome')
+        url = reverse("welcome")
         self.assertEqual(resolve(url).func.view_class, EventAPIView)
 
     def test_profile_url_is_resolved(self):
-        url = reverse('profile')
+        url = reverse("profile")
         self.assertEqual(resolve(url).func, profile)
