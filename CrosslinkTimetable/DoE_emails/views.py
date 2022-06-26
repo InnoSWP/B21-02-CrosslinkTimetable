2209f5a970cd4ff7813d213f18e70313718c28a6
Ilnar <4sitescarp@gmail.com>
2022-06-26 20:42:11 +0300

    Added students to database

--- CrosslinkTimetable/DoE_emails/views.py
+++ CrosslinkTimetable/DoE_emails/views.py
@@ -4,15 +4,27 @@ from rest_framework.response import Response
 from exchangelib import Message, Mailbox, Credentials, Account, DELEGATE
 from credentials import login, password
 
+from sqlite3 import connect
+
+
 class DoEEmailsAPIView(APIView):
     def post(self, request):
         group_of_recipients = request.data['group_of_recipients']
         group_of_recipients_cc = request.data['group_of_recipients_cc']
 
-        attendees_students = ['i.orekhov@innopolis.university', 'd.alekhin@innopolis.university',
-                     'i.ezhova@innopolis.university']
+        conn = connect('../CrosslinkTimetable/db.sqlite3')
+        cursor = conn.cursor()
+
         if group_of_recipients == 'Students':
-            group_of_recipients = list(map(lambda x: Mailbox(email_address=x), attendees_students))
+            receiver = list(map(lambda x: x[0], cursor.execute("SELECT * FROM Students").fetchall()))
+            group_of_recipients = list(map(lambda x: Mailbox(email_address=x), receiver))
+        elif group_of_recipients == 'Teachers':
+            receiver = list(map(lambda x: x[0], cursor.execute("SELECT * FROM Teachers").fetchall()))
+            group_of_recipients = list(map(lambda x: Mailbox(email_address=x), receiver))
+
+        print(group_of_recipients)
+
+        conn.close()
 
         subject = request.data['subject']
         content = request.data['content']
@@ -28,7 +40,7 @@ class DoEEmailsAPIView(APIView):
             subject=subject,
             body=content,
             to_recipients=group_of_recipients,
-            cc_recipients=['carl@example.com', 'denice@example.com'],
+            # cc_recipients=['carl@example.com', 'denice@example.com'],
         )
         m.send()
 
