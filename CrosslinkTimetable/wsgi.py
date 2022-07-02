695be01008f4a83b0f81bacaae0cbfbdff3eccd0
Ilnar <4sitescarp@gmail.com>
2022-07-02 15:33:42 +0300

    Changed urls and wsgi

--- CrosslinkTimetable/wsgi.py
+++ CrosslinkTimetable/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CrosslinkTimetable.CrosslinkTimetable.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CrosslinkTimetable.settings')
 
 application = get_wsgi_application()
