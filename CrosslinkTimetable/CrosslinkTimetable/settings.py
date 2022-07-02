4487440e177dd96581ca0c7e9b905bbdcb7bbd6b
Ilnar <4sitescarp@gmail.com>
2022-07-02 15:24:15 +0300

    Added whitenoise and changes static root

--- CrosslinkTimetable/CrosslinkTimetable/settings.py
+++ CrosslinkTimetable/CrosslinkTimetable/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware"
 ]
 
 ROOT_URLCONF = "CrosslinkTimetable.urls"
@@ -124,7 +125,8 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
-STATIC_ROOT = BASE_DIR / "../static"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATIC_ROOT = BASE_DIR / "../static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
